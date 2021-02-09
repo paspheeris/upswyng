de0b88c320390a53f0261d016d2d4915e7d8d216
paspheeris <pa.spheeris@gmail.com>
2021-02-08 17:04:54 -0700

    Tests working, building broken

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,9 +2,21 @@ module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
+    "^.+\\.mjs$": "babel-jest",
+    "^.+\\.svelte$": [
+      "svelte-jester",
+      {
+        "preprocess": true
+      }
+    ],
   },
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
-  moduleFileExtensions: ["ts", "js"],
+  moduleFileExtensions: ["ts", "js", "svelte", "mjs"],
   modulePathIgnorePatterns: ["node_modules"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleDirectories: ["node_modules", "src/node_modules"],
+	transformIgnorePatterns: [
+		'/node_modules/(?!@sapper)'
+	],
 };
